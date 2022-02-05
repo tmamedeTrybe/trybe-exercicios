924b8263edd95cf4d25f7f317522ec3d10f1e5bf
Thiago Mamede <tmamede2@gmail.com>
2022-02-05 03:19:25 -0200

    Adiciona resolucao de exercicios

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -1,14 +1,32 @@
 // Exercicio 01
-let fatorial = 1;
+/*let fatorial = 1;
 for (let index = 10; index > 0; index -= 1){
     fatorial *= index;
     }
-console.log(fatorial);
+console.log(fatorial);*/
 
 // Exercicio 02
-let word = 'tryber';
+/*let word = 'Thiago';
 let inverso = '';
-for (let index = 5; index <= word.length; index -= 1){
-    word.push(index);
+for (let index = word.length - 1; index >= 0; index -= 1){
+    inverso += word[index];
 }
-console.log(inverso)
+console.log(inverso);*/
+
+// Exercicio 03
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0];
+let menorPalavra = array [0];
+for (let index = 0; index < array.length; index += 1){
+    if (array[index].length > maiorPalavra.length){
+        maiorPalavra = array[index];
+    }
+}
+for (let index = 0; index < array.length; index += 1){
+    if (array[index].length < menorPalavra.length){
+        menorPalavra = array[index];
+    }
+}
+
+console.log (maiorPalavra);
+console.log (menorPalavra);
