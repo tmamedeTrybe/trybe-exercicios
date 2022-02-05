e9290e2760244cc4ac114c58ed6881aacc6de30c
Thiago Mamede <tmamede2@gmail.com>
2022-02-05 03:15:51 -0200

    Adiciona resolucao de exercicios

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scriptArrayFor.js
@@ -0,0 +1,85 @@
+/*let numbers = [5, 9, 4, 17, 70, 8, 100, 2, 36, 28];
+// Exercicio 01
+
+for (let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index])};
+
+// Exercicio 02
+let soma = 0;
+for (let index = 0; index < numbers.length; index +=1){
+    soma += numbers[index];
+}
+console.log(soma);
+
+// Exercicio 03
+let media = soma / numbers.length;
+console.log (media);
+
+// Exercicio 04
+if (media > 20){
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor que 20');s
+}
+
+// Exercicio 05
+let maiorNumero = numbers[0];
+for (let index = 0; index < numbers.length; index += 1){
+    if (numbers[index] > maiorNumero) {
+        maiorNumero = numbers[index];
+    }
+}
+console.log (maiorNumero);
+
+// Exercicio 06
+let numerosImpares = 0;
+for(let index = 0; index < numbers.length; index +=1){
+    if(numbers[index] %2 == 1){
+        numerosImpares += 1;
+    }
+}
+    if (numerosImpares === 0){
+        console.log ('Nenhum valor impar encontrado')
+    }
+console.log(numerosImpares);
+
+// Exercicios 07
+let menorValor = numbers[0];
+for (let index = 0; index < numbers.length; index += 1){
+    if (numbers[index] < menorValor) {
+        menorValor = numbers[index];
+    }
+}
+console.log (menorValor);
+
+// Exercicios 08
+let numeros = [];
+for (let index = 1; index <= 25; index += 1){
+    numeros.push (index);
+}
+console.log (numeros);
+
+// Exercicio 09
+for (let index = 0; index < numeros.length; index += 1){
+    console.log(numeros[index] /2);
+}
+*/
+// Bonus
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercicio 01 Bonus
+
+
+//Exercicios 02 Bonus
+
+
+// Exercicio 03 Bonus
+/*let resultado = [];
+for (let index3 = 0; index3 < numbers.length; index3 += 1){
+    if(index3 +1 < numbers.length) {
+        resultado.push (numbers[index3] * numbers[index3+1]);}
+        else {
+            resultado.push (numbers[index3] * 2);
+        }   
+    } 
+    console.log(resultado);*/
