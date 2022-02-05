0322987d7b07bc85a684a1e30653681f8d4e6b42
Thiago Mamede <tmamede2@gmail.com>
2022-02-05 03:42:43 -0200

    Adiciona resluçoes de execicios

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scriptArrayFor.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scriptArrayFor.js
@@ -1,20 +1,62 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-/*console.log(numbers);
+let numbers = [5, 9, 4, 17, 70, 8, 100, 2, 36, 28];
+/*// Exercicio 01
+*console.log(numbers);
 for (let index = 0; index < numbers.length; index += 1){
     console.log(numbers[index])};*/
 
-let soma = 0;
+// Exercicio 02
+/*let soma = 0;
 for (let index = 0; index < numbers.length; index +=1){
     soma = (numbers[index] + soma);
     
 }
+// Exercicio 03
 console.log(soma);
 
+// Exercicio 04
 let media = soma / numbers.length;
 console.log (media);
 if (media > 20){
     console.log('Valor maior que 20');
 } else {
-    console.log('Valor menor que 20');
+    console.log('Valor menor que 20');s
 }
+
+// Exercicio 05 */
+
+// Exercicio 06
+/*let numerosImpares = 0;
+for(let index = 0; index < numbers.length; index +=1){
+    if(numbers[index] %2 == 1){
+        numerosImpares += 1;
+    
+    }
+}
+    if (numerosImpares === 0){
+        console.log ('Nenhum valor impar encontrado')
+    }
+console.log(numerosImpares); */
+
+// Exercicios 07
+
+
+// Exercicios 08
+
+let numeros = [];
+for (let index = 1; index <= 25; index += 1){
+    numeros.push (index);
+}
+console.log (numeros);
+
+// Exercicio 09
+
+for (let index = 0; index < numeros.length; index += 1){
+    console.log(numeros[index] /2);
+}
+
+
+
+
+
+
 
