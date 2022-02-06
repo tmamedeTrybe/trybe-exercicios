8f0535de4c23053cc15fe7c450114963aa20eb8c
Thiago Mamede <tmamede2@gmail.com>
2022-02-06 03:58:31 -0200

    Ainda tentando resolver exercicio 4

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -14,7 +14,7 @@ for (let index = word.length - 1; index >= 0; index -= 1){
 console.log(inverso);*/
 
 // Exercicio 03
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+/*let array = ['java', 'javascript', 'python', 'html', 'css'];
 let maiorPalavra = array[0];
 let menorPalavra = array [0];
 for (let index = 0; index < array.length; index += 1){
@@ -29,4 +29,21 @@ for (let index = 0; index < array.length; index += 1){
 }
 
 console.log (maiorPalavra);
-console.log (menorPalavra);
+console.log (menorPalavra);*/
+
+// Exercicio 04
+
+let maiorNumeroPrimo = 0;
+let contador = 0;
+for (let index = 0; index <= 50; index += 1){
+    for (let divisor = 1; divisor <= index; divisor += 1){
+    if (index % divisor === 0){
+        contador += 1;
+        if (contador === 2){
+            maiorNumeroPrimo = index;
+        }
+    }
+}
+}
+
+console.log(maiorNumeroPrimo);
