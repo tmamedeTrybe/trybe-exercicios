014ef93ad560b660b72cb6bc883af3d2b7946839
Thiago Mamede <tmamede2@gmail.com>
2022-02-07 09:21:21 -0200

    Adiciona resolucao de exercicios 08 a 10 do dia 4.4

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -36,7 +36,7 @@
       console.log(info[key] + ' e ' + infoTio[key]);
     }
   }
-  */
+  
 
   let leitor = {
     nome: 'Julia',
@@ -65,5 +65,60 @@
  // Exercicio 08
 
  console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos.");
+*/
+
+//Exercicio 08
+
+/*let word = 'thiago';
+let splitWord = word.split('');
+console.log(splitWord);
+let reverseWord = splitWord.reverse();
+console.log(reverseWord);
+let joinWord = reverseWord.join('');
+console.log(joinWord);
+console.log(word);
+console.log(word === joinWord);*/
+
+
+function verificaPalindromo (palavra){
+    let palavraReversa = '';
+   for (let index = palavra.length - 1; index >= 0; index -= 1){
+         palavraReversa += palavra[index];
+    }
+    if (palavraReversa === palavra){
+        return 'true';
+    } else{ 
+    return 'false';} 
+}
+console.log(verificaPalindromo('amam'));
+
+// Exercicio 9
+
+function retorneIndiceMaiorValor(array){
+    let maiorValor = [0];
+    for (let index = 0; index < array.length; index += 1){
+        if (array[index] > maiorValor){
+            maiorValor = array[index];
+        }
+        
+    }
+    return array.indexOf(maiorValor);
+}
+console.log(retorneIndiceMaiorValor([210, 450, 6, 80, 10, 1001]));
+
+// Exercicio 10
+
+
+function retorneIndiceMenorValor(array){
+    let menorValor = [0];
+    for (let index = 0; index < array.length; index += 1){
+        if (array[index] < menorValor){
+            menorValor = array[index];
+        }
+        
+    }
+    return array.indexOf(menorValor);
+}
+console.log(retorneIndiceMenorValor([2, 4, 6, 7, 10, 0, -3]));
 
 
