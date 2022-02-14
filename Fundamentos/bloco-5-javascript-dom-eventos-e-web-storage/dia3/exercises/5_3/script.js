7d2547493aada808f1bd28e8a8725c9ba8678c58
Thiago Mamede <tmamede2@gmail.com>
2022-02-14 16:08:31 -0200

    Adiciona resolucao exercicios 01 e 02

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia3/exercises/5_3/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia3/exercises/5_3/script.js
@@ -1,5 +1,5 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDays = ['Domo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
   
     for (let index = 0; index < weekDays.length; index += 1) {
@@ -9,49 +9,34 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
-  };
+};
   
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
+  
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const container = document.querySelector('#days');
 
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  function createNumberDaysMonth (){
-   const numbersDaysContainer = document.querySelector('#days');
-
-  for (let index2 = 0; index2 < dezDaysList.length; index2 += 1){
-        const numbersDay = dezDaysList[index2];
-        const numbersDayItem = document.createElement('li');
-        numbersDayItem.innerHTML = numbersDay;
-        numbersDayItem.className = 'day';
-        if (numbersDay == 24 || numbersDay == 25 || numbersDay == 31){
-            numbersDayItem.classList.add('holiday');
-        } if (numbersDay == 4 || numbersDay == 11 || numbersDay == 18 || numbersDay == 25){
-            numbersDayItem.classList.add('friday')
-        }
-
-        numbersDaysContainer.appendChild(numbersDayItem);
-  }
-}
-
-createNumberDaysMonth();
-
-function createButton(string){
-    let buttonContainer = document.querySelector(".buttons-container");
-    let button = document.createElement('button');
-    button.id = ("btn-holiday");
-    button.innerHTML = string;
-
-    buttonContainer.appendChild(button);
-
-}
-createButton('Feriados');
-
-function changeColorHolidays (event){
-    let holidays = document.querySelectorAll('.holiday');
-    for (let index = 0; index < holidays.length; index =+ 1){
-        holidays[index].style.backgroundColor = 'rgb(238,238,238)';
+function createDaysOfTheMonth (){
+  for (let index = 0; index < dezDaysList.length; index += 1){
+      let numberDays = dezDaysList[index];
+      let numberDaysItem = document.createElement('li');
+      numberDaysItem.innerHTML = numberDays;
+      numberDaysItem.className = 'day';
+      container.appendChild(numberDaysItem);
+      if (numberDays === 24 || numberDays === 25 || numberDays === 31){
+          numberDaysItem.classList.add('holiday');
+      } if (numberDays === 04 || numberDays === 11 || numberDays === 18 || numberDays === 25){
+        numberDaysItem.classList.add('friday');
+      }
     }
 }
-changeColorHolidays();
+createDaysOfTheMonth();
 
-button.addEventListener('click', changeColorHolidays);
-
+function createButton(string){
+    let containerButton = document.querySelector('.buttons-container');
+    let button = document.createElement('button');
+    button.id = 'btn-holiday';
+    button.innerText = string;
+    containerButton.appendChild(button);
+;}
+createButton('Feriados');
