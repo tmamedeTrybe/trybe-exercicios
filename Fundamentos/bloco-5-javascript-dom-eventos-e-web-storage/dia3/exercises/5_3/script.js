49fecc37378e3f2be8d749839e915209178ec97d
Thiago Mamede <tmamede2@gmail.com>
2022-02-15 11:04:06 -0200

    Adiciona resolucao exercicio 05

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia3/exercises/5_3/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia3/exercises/5_3/script.js
@@ -10,7 +10,8 @@ function createDaysOfTheWeek() {
       weekDaysList.appendChild(dayListItem);
     };
 };
-  
+
+
 createDaysOfTheWeek();
   
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
@@ -48,13 +49,33 @@ function changeHolidayColor() {
   let holidays = document.querySelectorAll('.holiday');
   for(let index = 0; index < holidays.length; index += 1){
        if(holidays[index].style.backgroundColor === 'white'){
-      holidays[index].style.backgroundColor = "rgb(238,238,238)";
-    } else{ holidays[index].style.backgroundColor = 'white'
-
+        holidays[index].style.backgroundColor = "rgb(238,238,238)";
+        }   else{ holidays[index].style.backgroundColor = 'white'
+            }
     }
-  
-  }
   }
   
+  function createButtonFriday (string){
+    let containerButtonFriday = document.querySelector(".buttons-container");
+    let buttonFriday = document.createElement('button');
+    buttonFriday.id = "btn-friday";
+    buttonFriday.innerHTML = string;
+    containerButtonFriday.appendChild(buttonFriday);
+  }
+  createButtonFriday('Sexta-feira');
 
+  let buttonFriday = document.querySelector('#btn-friday');
+  function changeTextFriday (){
+      let fridays = document.getElementsByClassName('friday');
+      let dezFridays = [4,11,18,25];
+         for (let index = 0; index < fridays.length; index += 1){
+         if (fridays[index].innerHTML !== 'Sextou!' ){
+           fridays[index].innerHTML = 'Sextou!';
+         } else {fridays[index].innerHTML = dezFridays[index]}
+       
+  }
+}
   
+buttonFriday.addEventListener('click', changeTextFriday);
+
+
