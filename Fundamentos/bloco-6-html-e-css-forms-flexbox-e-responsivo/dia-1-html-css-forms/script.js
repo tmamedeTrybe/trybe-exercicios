e4c5a7e5fb3090ee180ebba542587c7fcf6f5049
Thiago Mamede <tmamede2@gmail.com>
2022-02-24 15:38:20 -0300

    aletracoes nos exercicios

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,10 +1,48 @@
 let nome = document.querySelector('#user-name');
 let email = document.querySelector('#user-email');
 let textarea = document.querySelector('#textarea');
+let botaoEnviar = document.querySelector('#button-enviar');
+let autorizo = document.querySelector('#check-uso-imagens');
 
-if(nome.value.length < 10 || nome.value.length > 50){
-    alert('Dados Invalidos');
-} if (email.value.length < 10 || email.value.length > 50){
-    alert('Dados Inválidos!');
-} 
+function habilitarBotaoEnviar(){
+    botaoEnviar.disabled = !autorizo.checked;
+}
 
+function verificaLength(){ 
+if(nome.value.length < 10 || nome.value.length > 40 || email.value.length < 10 || email.value.length > 50 || textarea.value.length > 500){
+    alert('Dados Invalidos!');
+} else{
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+}
+}
+import JustValidate from 'just-validate';
+const validate = new JustValidate('#form');
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ]);
+
+window.onload = function(){
+botaoEnviar.addEventListener('click', verificaLength);
+autorizo.addEventListener('change', habilitarBotaoEnviar)
+
+}
