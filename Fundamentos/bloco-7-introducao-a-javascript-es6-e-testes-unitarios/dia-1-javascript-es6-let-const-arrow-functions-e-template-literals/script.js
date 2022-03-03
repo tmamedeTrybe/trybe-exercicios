d12461d91508e30092a97cb1006d24b3b44271f0
Thiago Mamede <tmamede2@gmail.com>
2022-03-03 15:34:00 -0300

    Resolucao de exercicio 01 do dia 7.1

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,17 @@
+    //   function testingScope(escopo) {
+    //     if (escopo === true) {
+    //       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    //       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    //       console.log(ifScope);
+    //     } else {
+    //       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    //       console.log(elseScope);
+    //     }
+    //     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+    //   }
+
+    //   testingScope(true);
+
+      const testingScope = (escopo) => escopo === true ? `Não devo ser utilizada fora do meu escopo`: `òtimo fui utilizda no meu escopo`;
+      console.log(testingScope(false));
+
