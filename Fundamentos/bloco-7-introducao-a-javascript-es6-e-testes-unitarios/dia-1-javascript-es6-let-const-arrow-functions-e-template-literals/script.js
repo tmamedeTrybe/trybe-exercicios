6fa97db56bd4334adbafdb1784e1d0704a4e9f20
Thiago Mamede <tmamede2@gmail.com>
2022-03-03 20:57:38 -0300

    Adiciona resolucao de exercicios part01 completa e 01 e 02 da parte 02

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,17 +1,50 @@
-    //   function testingScope(escopo) {
-    //     if (escopo === true) {
-    //       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    //       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    //       console.log(ifScope);
-    //     } else {
-    //       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    //       console.log(elseScope);
-    //     }
-    //     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-    //   }
 
-    //   testingScope(true);
+//PARTE I
+//Exercicio 01
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(`${ifScope} o que estou fazendo aqui ? `);
+  }
+  testingScope(true);
 
-      const testingScope = (escopo) => escopo === true ? `Não devo ser utilizada fora do meu escopo`: `òtimo fui utilizda no meu escopo`;
-      console.log(testingScope(false));
+// Exercício 02
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const retornaArrayCrescente = (array) => array.sort((a,b) => a -b);
+console.log(`"Os números ${retornaArrayCrescente(oddsAndEvens)} se encontram ordenados de forma crescente!"`)
+
+// PARTE II
+// Exercício 01
+
+function fatorial(number){
+    let fatorialNumber = 1;
+    for (let index = 1; index <= number; index += 1){
+        fatorialNumber *= index;
+    }
+    return fatorialNumber;
+   
+}
+console.log(fatorial(5));
+
+// Exercicio 02
+
+const retornaMaiorPalavra = (frase) => {
+    let fraseArray =  frase.split(" ");
+    let maiorPalavra = fraseArray[0];
+    for (let index = 0; index < fraseArray.length; index += 1){
+        if (fraseArray[index].length > maiorPalavra.length) {
+            maiorPalavra = fraseArray[index]}
+    }
+    return maiorPalavra;
+    
+}
+
+console.log(retornaMaiorPalavra('Antônio foi no banheiro e não sabemos o que aconteceu'))
 
