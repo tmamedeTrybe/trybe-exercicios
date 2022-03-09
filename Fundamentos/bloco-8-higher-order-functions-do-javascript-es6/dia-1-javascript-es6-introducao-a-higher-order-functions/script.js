a8ebf4b899b443aa768c01f7ea608c3da5f503f3
Thiago Mamede <tmamede2@gmail.com>
2022-03-09 18:16:24 -0300

    Resolução do exercicio 02

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,18 +1,38 @@
-const newEmployees = (novoContratado) => {
-    const employees = {
-      id1: novoContratado('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: novoContratado('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: novoContratado('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    }
-    return employees;
-  };
+// const newEmployees = (novoContratado) => {
+//     const employees = {
+//       id1: novoContratado('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//       id2: novoContratado('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//       id3: novoContratado('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//     }
+//     return employees;
+//   };
 
-novoContratado = (nomeCompleto) => {
-    const email = nomeCompleto.toLowerCase().split(' ').join('_');
-    return {nomeCompleto, email: `${email}@trybe.com`};
+// novoContratado = (nomeCompleto) => {
+//     const email = nomeCompleto.toLowerCase().split(' ').join('_');
+//     return {nomeCompleto, email: `${email}@betrybe.com`};
+// }
+
+// console.log(newEmployees(novoContratado));
+
+const verificaAcerto = (numeroApostado) => {
+    let numeroSorteado = Math.floor(Math.random() * 60 + 1);
+    console.log(numeroSorteado);
+    if (numeroApostado === numeroSorteado) {
+    return true
+    } return false;
+};
+
+const resultadoSorteio = (callback, numeroApostado) =>{
+    if ( callback(numeroApostado) === true){
+        return "Parabens!!!";
+    } return "Tente novamente.";
 }
 
-console.log(newEmployees(novoContratado));
+console.log(resultadoSorteio(verificaAcerto, 18));
+
+
+
+
 
 
 
