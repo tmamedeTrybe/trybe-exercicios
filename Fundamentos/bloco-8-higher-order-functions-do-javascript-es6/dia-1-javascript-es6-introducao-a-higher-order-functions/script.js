588c9b1bb06b9904a013d9d6a7a1c4f4351f1981
Thiago Mamede <tmamede2@gmail.com>
2022-03-09 19:22:08 -0300

    Resolucao exercicio 03

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,3 +1,5 @@
+// Exercicio 01
+
 // const newEmployees = (novoContratado) => {
 //     const employees = {
 //       id1: novoContratado('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
@@ -14,22 +16,49 @@
 
 // console.log(newEmployees(novoContratado));
 
-const verificaAcerto = (numeroApostado) => {
-    let numeroSorteado = Math.floor(Math.random() * 60 + 1);
-    console.log(numeroSorteado);
-    if (numeroApostado === numeroSorteado) {
-    return true
-    } return false;
-};
+// Exercicio 02
 
-const resultadoSorteio = (callback, numeroApostado) =>{
-    if ( callback(numeroApostado) === true){
-        return "Parabens!!!";
-    } return "Tente novamente.";
+// const verificaAcerto = (numeroApostado) => {
+//     let numeroSorteado = Math.floor(Math.random() * 60 + 1);
+//     console.log(numeroSorteado);
+//     if (numeroApostado === numeroSorteado) {
+//     return true
+//     } return false;
+// };
+
+// const resultadoSorteio = (callback, numeroApostado) =>{
+//     if ( callback(numeroApostado) === true){
+//         return "Parabens!!!";
+//     } return "Tente novamente.";
+// }
+
+// console.log(resultadoSorteio(verificaAcerto, 18));
+
+//Exercicio 03
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const pontuacaoEstudante = (callback, respostasCertas, gabarito) => {
+    let pontuacaoEstudante = 0;
+    for (let index = 0; index < respostasCertas.length; index += 1){
+        let resposta = callback(respostasCertas[index], gabarito[index]);
+        pontuacaoEstudante += resposta;
+    }
+    return pontuacaoEstudante;
+
 }
 
-console.log(resultadoSorteio(verificaAcerto, 18));
+const checaRespostas = (respostaCertas, gabarito) => {
+    if(respostaCertas === gabarito){
+        return 1
+    } else if (gabarito === 'N.A'){
+        return 0
+    } else { return -0.5 }
 
+}
+
+console.log(pontuacaoEstudante(checaRespostas, RIGHT_ANSWERS, STUDENT_ANSWERS));
 
 
 
